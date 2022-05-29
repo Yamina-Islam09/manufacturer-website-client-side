f4ce52b50f4b3e6178005949b604df2d7f3c1c93
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-29 15:17:25 +0600

    admin portion done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,13 +6,20 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: REACT_APP_apiKey,
-  authDomain: REACT_APP_authDomain,
-  projectId: REACT_APP_projectId,
-  storageBucket: "assignment-12-cd70c.appspot.com",
-  messagingSenderId: "324271992829",
-  appId: "1:324271992829:web:18de54d4302bca3045b394"
 
+  // apiKey:process.env.REACT_APP_apiKey,
+  // authDomain:process.env.REACT_APP_authDomain,
+  // projectId:process.env.REACT_APP_projectId,
+  // storageBucket:process.env.REACT_APP_storageBucket,
+  // messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  // appId:process.env.REACT_APP_appId,
+
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 
 
   // apiKey: "AIzaSyD22523AVZE-cNGjKWtsnRIKn5eMtRuA3g",
